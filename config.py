099a3ec1ff67e5e8a818ed44ec9a7d9dd15604dd
hanos1987 <benjfeir@protonmail.com>
2025-03-28 19:17:40 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+reddit_config = {
+    "client_id": "YE_9ojlCFZvn_Fx9b3S4HA",
+    "client_secret": "y96V4HbFQtnX4oatLDXj0vfZ7tHx0Q",
+    "username": "The_Hanos",
+    "password": "R12345eddit09876!",
+    "user_agent": "Modmail AI Bot v1.0"
+}
+
+openai_config = {
+    "api_key": "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"
+}
